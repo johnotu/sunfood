6295602b7d151821c25d5c2bba39a8a80fc05352
oshka <olkace@gmail.com>
2018-11-04 22:53:44 +0200

    main page content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import api from './utils/api'
 import sortByDate from './utils/sortByDate'
 import isLocalHost from './utils/isLocalHost'
 import './App.css'
+import food from './assets/food.png'
+
 
 export default class App extends Component {
   state = {
@@ -278,32 +280,20 @@ export default class App extends Component {
       <div className='app'>
 
         <AppHeader />
+        <div  className="main-home">
 
-        <div className='todo-list'>
-          <h2>
-            Create todo
-            <SettingsIcon onClick={this.openModal} className='mobile-toggle' />
-          </h2>
-          <form className='todo-create-wrapper' onSubmit={this.saveTodo}>
-            <input
-              className='todo-create-input'
-              placeholder='Add a todo item'
-              name='name'
-              ref={el => this.inputElement = el}
-              autoComplete='off'
-              style={{marginRight: 20}}
-            />
-            <div className='todo-actions'>
-              <button className='todo-create-button'>
-                Create todo
-              </button>
-              <SettingsIcon onClick={this.openModal}  className='desktop-toggle' />
+            <h2><span>Sun</span>Code<span>Flower</span> presents..</h2>
+            <p>We all love tasty food and eat well. But sometimes we lack the imagination or words to describe what we want for dinner.</p>
+            <p>We present you a mega application using the latest technologies that can solve all the above problems.</p>
+            <p>In our great app we use only modern API such as Fauna (for call the db and receive data), Clarify (to detect food image), Formspree (for collecting delivery address), Pilon (because it's interesting to try). And many, many others.</p>
+            <p>Our goal is to get acquainted with the above mentioned API, to determine the possibilities of their further application in our future projects, to find git  for ourselves API's strengths and weaknesses.</p>
+            <p>Meet ...</p>
+            <h1 style={{ textAlign:'center', }}><a href=""><span>Sun</span>Food<span>App</span></a></h1>
+            <div style={{ maxWidth: '100%', margin: 'auto', textAlign:'center', }}>
+              <img src={food} alt="Logo" />
             </div>
-          </form>
-
-          {this.renderTodos()}
-        </div>
-        <SettingsMenu
+      </div>
+    <SettingsMenu
           showMenu={this.state.showMenu}
           handleModalClose={this.closeModal}
           handleClearCompleted={this.clearCompleted}
